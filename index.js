c7cdc346b1ebe5c7dd2b24d99a488883a68badcc
walid allan <walid.allane@outlook.com>
2022-10-10 11:07:18 +0100

    developing in the calculator

---
+++ index.js
@@ -0,0 +1,39 @@
+const screenResulte = document.querySelector('.screenResulte');
+const btns = Array.from(document.querySelectorAll('.btn'));
+
+// console.log(btns);
+btns.map( btn => {
+  btn.addEventListener('click', (e) => {
+    switch (e.target.innerText) {
+      case 'AC':
+         screenResulte.innerText = "";
+        break;
+        case 'CE':
+          if (screenResulte.innerText) {
+            screenResulte.innerText = screenResulte.innerText.slice(0, -1);
+          }
+          case '=':
+            try {
+              screenResulte.innerText = eval(screenResulte.innerText);
+
+                let activeButton = document.querySelector(".equalBtn");
+
+                activeButton.classList.add('pressed');
+
+                setTimeout(function(){
+                   activeButton.classList.remove('pressed');
+                }, 60);
+
+            } catch {
+              screenResulte.innerText = "ERROR";
+            }
+            break;
+          break;
+      default:
+        screenResulte.innerText += e.target.innerText;
+        if (btns >= 18) {
+          screenResulte.innerText;
+        }
+    }
+  });
+});
