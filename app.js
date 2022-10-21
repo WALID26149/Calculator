8079296c4fbbeada6553dc98df79cb7ad329b48d
walid allane <walid.allane@outlook>
2022-10-21 17:04:57 +0100

    commit num 12

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const favicon = require('server-favicon');
-
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('public'));
-app.use(favicon(__dirname + "/favicon.io.png"))
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
